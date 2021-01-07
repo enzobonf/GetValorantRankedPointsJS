48a8abb1024668c7f0fb5c0ba9919b56e576a388
enzobonf <52019749+enzobonf@users.noreply.github.com>
2021-01-06 23:32:44 -0300

    Fix auth error

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const auth = (authData) => {
     let data = {
       'client_id': 'play-valorant-web-prod',
             'nonce': '1',
-            'redirect_uri': 'https://beta.playvalorant.com/opt_in',
+            'redirect_uri': 'https://playvalorant.com/opt_in',
             'response_type': 'token id_token',
     };
 
